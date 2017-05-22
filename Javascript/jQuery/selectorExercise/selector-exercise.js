8dbb8658c1bf44ac77996b813b51e56d87b09b4a
Kevin Balderston <kbalderston@gmail.com>
2017-05-22 13:29:34 -0700

    Added jQuery exercise files

---
+++ Javascript/jQuery/selectorExercise/selector-exercise.js
@@ -0,0 +1,8 @@
+//select all divs and make their background purple
+$("div").css("background", "purple");
+//select the divs with class highlight and make them 200px wide
+$("div.highlight").css("width", "200px");
+//select the div with id third and give it a orange border
+$("#third").css("border", "solid 1px orange");
+//select only first div and change font color to pink
+$("div:first-of-type").css("color", "pink");
